56d06ec1c56e022ab5c2347900050a5a871726be
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2022-07-30 16:37:12 +0530

    updated Header

--- resume/src/container/Header/Header.jsx
+++ resume/src/container/Header/Header.jsx
@@ -6,15 +6,15 @@ const Header = () => {
     <>
       <section id="home" className="header">
         <div className="container">
-          <div className="row min-vh-100 align-items-center">
+          <div className="row">
             <div className="col-lg-7">
-              <div className="header">
+              {/* <div className="header"> */}
                 <h6 className="header__hello">Hello. </h6>
                 <h1 className="header__intro">I'm Sahib Singh</h1>
                 <p className="header__list">
                   <b className="yellow-tags">&lt;</b>He/Him <b className="yellow-tags">/&gt;</b>
                 </p>
-              </div>
+              {/* </div> */}
             </div>
             <div className="col-lg-5">
               <div className="profile-pic">

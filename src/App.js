3cc4c62d7856776a6b8bd9714a4104d6762d1d86
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2022-10-19 10:35:24 +0530

    Updated Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import React from 'react'
-import { About, Footer, Header, Project, Skills, Education, FormikContact } from './container';
-import { Navbar } from './components';
+import React from "react";
+import {
+  About,
+  Footer,
+  Header,
+  Project,
+  Skills,
+  Education,
+  FormikContact,
+} from "./container";
+import { Navbar } from "./components";
 
-import './App.scss'
+import "./App.scss";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
-      <Header />
-      <About />
-      <Project />
-      <Skills />
-      <Education />
-      {/* <Contact /> */}
-      <FormikContact />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <Header />
+                <About />
+                <Project />
+                <Skills />
+                <Education />
+                <FormikContact />
+              </>
+            }
+          />
+          <Route path="*" element={ <Navigate to="/" /> } />
+        </Routes>
+      </BrowserRouter>
       <Footer />
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;

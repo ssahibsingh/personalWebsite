04259d21321a27a6b98a09309bf07e8d4e274883
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2022-10-25 16:59:55 +0530

    Updated Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,62 +1,158 @@
-import React, { useState } from 'react';
-import { images } from '../../constants';
-import './Navbar.scss';
-import Button from '@cred/neopop-web/lib/components/Button';
-import resume from '../../assets/resume/Resume - Sahib Singh.pdf'
+import { useRef, useState } from "react";
+import "./Navbar.scss";
+import Button from "@cred/neopop-web/lib/components/Button";
+import resume from "../../assets/resume/Resume - Sahib Singh.pdf";
 
 const Navbar = () => {
-  const [isNavExpanded, setIsNavExpanded] = useState(false);
   const [isOver, setIsOver] = useState(false);
+  const navButton = useRef(null);
+  const linksContainerRef = useRef(null);
 
-  const colorConfig = {
-    backgroundColor: '#F7AF24',
-    borderColor: '#18171C',
-    edgeColors: {
-      left: 'transparent',
-      top: 'transparent',
-      right: '#18171C',
-      bottom: '#18171C'
-    },
-    color: '#18171C'
+  function collapseNav() {
+    navButton.current.classList.add("collapsed");
+    linksContainerRef.current.classList.remove("show");
   }
 
+  const colorConfig = {
+    backgroundColor: "#F7AF24",
+    borderColor: "#18171C",
+    edgeColors: {
+      left: "transparent",
+      top: "transparent",
+      right: "#18171C",
+      bottom: "#18171C",
+    },
+    color: "#18171C",
+  };
+
   return (
-    <>
-      <nav className="nav">
-      <div className="nav__logo">
-        <h1><a href="/">Sahib Singh</a></h1>
-      </div>
-        
-        <button className="hamburger" onClick={() => { setIsNavExpanded(!isNavExpanded) }}>
-          <i className="fa-solid fa-bars"></i>
+    <nav className="navbar navbar-expand-lg shadow-sm">
+      <div className="container-fluid">
+        <div className="navbar-brand d-flex align-items-center mx-3 pt-3">
+          <h1>
+            <a onClick={collapseNav} className="text-decoration-none" href="/">
+              Sahib Singh
+            </a>
+          </h1>
+        </div>
+        <button
+          ref={navButton}
+          className="navbar-toggler collapsed d-flex d-lg-none flex-column justify-content-around"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          {/* <span className="navbar-toggler-icon"></span> */}
+
+          <span class="toggler-icon top-bar"></span>
+          <span class="toggler-icon middle-bar"></span>
+          <span class="toggler-icon bottom-bar"></span>
         </button>
-        <div className={isNavExpanded ? "nav__menu expanded" : "nav__menu"}>
-          <ul onClick={() => { setIsNavExpanded(false) }}>
-            <li><a className="nav-link" href="#home" >Home</a></li>
-            <li><a className="nav-link" href="#about">About</a></li>
-            <li><a className="nav-link" href="#project">Projects</a></li>
-            <li><a className="nav-link" href="#skill">Skills & Experience</a></li>
-            <li><a className="nav-link" href="#education">Education</a></li>
-            <li><a className="nav-link" href="#contact">Contact</a></li>
-            
-            <li><a className="nav__btn nav-link nav__menu-btn" href={resume} target="_blank" rel="noreferrer" onMouseOver={() => { setIsOver(true) }} onMouseOut={() => { setIsOver(false) }}>
-            <Button
-              className={isOver && "active"}
-              colorConfig={colorConfig}
-              variant="primary" kind="elevated" size="medium" colorMode="dark">Get Resume</Button>
-          </a></li>
+        <div
+          ref={linksContainerRef}
+          className="collapse navbar-collapse"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav m-auto mb-2 mb-lg-0 text-center">
+            <li className="nav-item">
+              <a onClick={collapseNav} className="nav-link px-3" href="#home">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a onClick={collapseNav} className="nav-link px-3" href="#about">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                onClick={collapseNav}
+                className="nav-link px-3"
+                href="#project"
+              >
+                Projects
+              </a>
+            </li>
+            <li className="nav-item">
+              <a onClick={collapseNav} className="nav-link px-3" href="#skill">
+                Skills & Experience
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                onClick={collapseNav}
+                className="nav-link px-3"
+                href="#education"
+              >
+                Education
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                onClick={collapseNav}
+                className="nav-link px-3"
+                href="#contact"
+              >
+                Contact
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href={resume}
+                onClick={collapseNav}
+                className="text-decoration-none navbar-nav-btn"
+                target="_blank"
+                rel="noreferrer"
+                onMouseOver={() => {
+                  setIsOver(true);
+                }}
+                onMouseOut={() => {
+                  setIsOver(false);
+                }}
+              >
+                <Button
+                  className={isOver && "active"}
+                  colorConfig={colorConfig}
+                  variant="primary"
+                  kind="elevated"
+                  size="medium"
+                  colorMode="dark"
+                >
+                  Get Resume
+                </Button>
+              </a>
+            </li>
           </ul>
         </div>
-        <div className="ms-auto">
-          <a className="nav__btn " href={resume} target="_blank" rel="noreferrer" onMouseOver={() => { setIsOver(true) }} onMouseOut={() => { setIsOver(false) }}>
-            <Button
-              className={isOver && "active"}
-              colorConfig={colorConfig}
-              variant="primary" kind="elevated" size="medium" colorMode="dark">Get Resume</Button>
-          </a>
-        </div>
-      </nav>
-    </>
+        <a
+          href={resume}
+          onClick={collapseNav}
+          className="text-decoration-none navbar-btn"
+          target="_blank"
+          rel="noreferrer"
+          onMouseOver={() => {
+            setIsOver(true);
+          }}
+          onMouseOut={() => {
+            setIsOver(false);
+          }}
+        >
+          <Button
+            className={isOver && "active"}
+            colorConfig={colorConfig}
+            variant="primary"
+            kind="elevated"
+            size="medium"
+            colorMode="dark"
+          >
+            Get Resume
+          </Button>
+        </a>
+      </div>
+    </nav>
   );
 };
 
